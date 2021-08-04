5c64057f46c8879337263efdf16b5b7a9e88bfe7
Ilnara Osokina <ilnara_19@mail.ru>
2021-08-04 22:46:49 +0700

    2-task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,24 @@
+let arr = [5, 3, 8, 1];
+
+
 function filterRange(arr, a, b) {
-  // ваш код...
+  let arrNew = [];
+
+  for (let i = 0; i < arr.length; i++){
+    
+    if ( arr[i] >= a && arr[i] <= b) {
+  
+      arrNew.push(arr[i]);
+    }
+  }
+  
+  return arrNew;
 }
+
+let filtered = filterRange(arr, 1, 4);
+
+console.log(filtered);
+
+console. log(arr);
+
+
