2f641e7094764066f9616a4d768ba11a1ddf0a1c
Ilnara Osokina <ilnara_19@mail.ru>
2021-08-08 23:30:05 +0700

    4-5 tasks

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,49 @@
-function showSalary(users, age) {
-  // ваш код...
+let user1 = {
+  balance: "$1,825.65",
+  picture: "https://placehold.it/32x32",
+  age: 21,
+  name: "Golden Branch",
+  gender: "male",
+  greeting: "Hello, Golden Branch! You have 7 unread messages.",
+  favouriteFruit: "banana"
+};
+
+let user2 = {
+  balance: "$3,825.65",
+  picture: "https://placehold.it/32x32",
+  age: 34,
+  name: "Golden John",
+  gender: "female",
+  greeting: "Hello, Golden John! You have 7 unread messages.",
+  favouriteFruit: "berry"
+};
+
+let users = [user1, user2];
+let age = 34;
+let str = [];
+let strNew;
+
+function  showSalary(users, age) {
+ 
+for (let i = 0; i < users.length; i++) {
+ 
+  if(users[i].age <=  age) {
+    str.push(users[i].name);
+    str.push(users[i].balance);
+    str.push(' \n');
+  }
+}  
+
+if(str.lastIndexOf('\n') != -1) {
+  let indexItem = str.lastIndexOf('\n');
+  str.splice(indexItem, 1);
 }
+
+   strNew = str.join(' '); 
+   return strNew;
+}
+
+
+let result = showSalary(users, age);
+
+console.log(result);
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,51 @@
+let str = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
+
+let arr = [];
+
+let arrNew = [];
+
+let arrNum = [];
+
+let num;
+
+let result = {};
+
 function getMinMax(str) {
-  // ваш код...
+  arr = str.split(' ');
+
+  for (let i = 0; i < arr.length; i++) {
+    num = Number( arr[i] );
+    arrNew.push(num);
+  }
+
+  for (let i = 0; i < arrNew.length; i++) {
+    
+    if(arrNew[i] = arrNew[i]) {
+      arrNum.push(arrNew[i]);
+    }
+  }
+
+  let arrMax = arrNew.slice();
+
+  for (let i = 0; i < arrNum.length; i ++) {
+    
+    if(arrNum[i] < arrNum[i+1] ) {
+      arrNum[i+1] = arrNum[i];
+    }
+
+    result.min =  arrNum[i];
+  }
+
+  for (let i = 0; i < arrMax.length; i ++) {
+    
+    if( arrMax[i] > arrMax[i+1] ) {
+      arrMax[i+1] = arrMax[i];
+    }
+
+    result.max =  arrMax[i];
+  }
+
+  return result;
 }
+
+console.log(getMinMax(str)); // { min: -5.8, max: 73  }
