5ff719f1a5d75a8f900cd7ced00d115059b8d833
Ilnara Osokina <ilnara_19@mail.ru>
2021-08-24 20:45:41 +0700

    3 task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -2,8 +2,12 @@ function initCarousel() {
   let arrowRight = document.querySelector('.carousel__arrow_right');
   let arrowLeft = document.querySelector('.carousel__arrow_left');
   let carouselInner = document.querySelector('.carousel__inner');
-  
+
   let widthSlider = 0;
+  if(widthSlider == 0) {
+    arrowLeft.style.display = 'none';
+  }
+
   arrowRight.addEventListener('click', function() {
     widthSlider = widthSlider + carouselInner.offsetWidth;
     if(widthSlider > carouselInner.offsetWidth*2) {
@@ -13,7 +17,7 @@ function initCarousel() {
     
     if(widthSlider > 0) {
       arrowLeft.style.display = '';
-    }
+    } 
   });
   
     arrowLeft.addEventListener('click', function() {
@@ -21,11 +25,11 @@ function initCarousel() {
       if(widthSlider > carouselInner.offsetWidth) {
         arrowRight.style.display = '';
       } 
-    carouselInner.style.transform = `translateX(${-widthSlider}px)`;
 
-    if(widthSlider == 0) {
-      arrowLeft.style.display = 'none';
-    }
+      if(widthSlider == 0) {
+        arrowLeft.style.display = 'none';
+      }
+    carouselInner.style.transform = `translateX(${-widthSlider}px)`;
   });
 }
 
