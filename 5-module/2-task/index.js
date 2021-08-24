9e5c51c9a63cc6ab873492784f08778f6f65b786
Ilnara Osokina <ilnara_19@mail.ru>
2021-08-24 17:36:46 +0700

    tasks

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,15 @@
 function toggleText() {
-  // ваш код...
+  
+  let toggler = document.querySelector('.toggle-text-button');
+  let buttonOpen = document.getElementById('text');
+
+
+    toggler.addEventListener( 'click', function() {
+      buttonOpen.setAttribute('hidden', 'hidden');
+      toggler.addEventListener( 'click', function() {
+        buttonOpen.removeAttribute('hidden', 'hidden');
+       });
+    });
 }
+
+    
